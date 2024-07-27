814b6b8efeb4ff93449ca1fcf73a5ab45dec888f
bhavik <bhavik.kawli@gmail.com>
2024-07-27 23:06:40 +0530

    trying cuda

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from transformers import pipeline
 from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
 
 
@@ -11,16 +12,23 @@ app = Flask(__name__)
 def use_model():
     payload = request.get_json()
     print(payload['prompt'])
-    tokenizer = AutoTokenizer.from_pretrained("./downloaded_models/TinyLlama-1.1B-Chat-v1.0")
-    model = AutoModelForCausalLM.from_pretrained("./downloaded_models/TinyLlama-1.1B-Chat-v1.0")
-    # pipe = pipeline("text-generation", model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
-    pipe = pipeline("text-generation",
-                    tokenizer=tokenizer,
-                    model=model,
-                    max_new_tokens=512,
-                    do_sample=True,
-                    temperature=0.9,
-                    repetition_penalty=1.1)
+    if torch.cuda.is_available():
+        print('cuda is available')
+    else:
+        print('cuda not available')
+
+    # tokenizer = AutoTokenizer.from_pretrained("./downloaded_models/TinyLlama-1.1B-Chat-v1.0")
+    # model = AutoModelForCausalLM.from_pretrained("./downloaded_models/TinyLlama-1.1B-Chat-v1.0")
+    # pipe = pipeline("text-generation", model="./downloaded_models/TinyLlama-1.1B-Chat-v1.0", device=torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    # pipe = pipeline("text-generation", model="./downloaded_models/TinyLlama-1.1B-Chat-v1.0", device=torch.device("cuda:0"))
+    pipe = pipeline("text-generation", model="./downloaded_models/TinyLlama-1.1B-Chat-v1.0", device="cuda")
+    # pipe = pipeline("text-generation",
+    #                 tokenizer=tokenizer,
+    #                 model=model,
+    #                 max_new_tokens=512,
+    #                 do_sample=True,
+    #                 temperature=0.9,
+    #                 repetition_penalty=1.1)
     return pipe(payload['prompt'])[0]['generated_text']
     # messages = [
     #     {"role": "user", "content": "Who are you?"},
